39c5611d56e66044c9bb0cbfca078fc90c4bffe9
Kazuma Zexs <90124265+CastyiGlitchxz@users.noreply.github.com>
2023-03-23 02:26:47 +0000

    More codes

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-function IsButton(Color, EqualClass, Typing, TravelURL) {
-  if (EqualClass == "SPBTN") {
-    Typing = "Side Panel Button"
-    
-  } else {
-    Typing = "Regular Button";
-    EqualClass = "No Class";
-    Color = "Not Defined";
-    TravelURL = "No URL";
-  }
+//Gets and Sets profile picture
+document.getElementById("ProfilePicture")
 
-  console.log(Color, EqualClass, Typing, TravelURL);
+ProfilePicture.src = localStorage.getItem("PFP");
+
+// Travel to the defined url inputted in the button [Index.html]
+function Travel(Url) {
+  TravelID = Url;
 }
 
+//TravelID equaled to Url for easy define
+var TravelID
+
+
+//Reloads IFrame so it will display correctly
+
+function SetLocation() {
+  document.getElementById("ViewportFrame").src = "https://BlueSkyezWebsite.amonguszbs10K.repl.co/" + "Viewport" + "/" + TravelID + ".html";
+}
+Travel;
